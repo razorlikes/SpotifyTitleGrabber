215efad5b2fbe34f2db9b12728aebd68d01f308e
razorlikes <jnieth@live.de>
2016-08-24 01:56:17 +0200

    Saving configuration into an extra file

--- SpotifyTitleGrabber/Form1.cs
+++ SpotifyTitleGrabber/Form1.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SpotifyTitleGrabber
 {
@@ -17,20 +18,58 @@ namespace SpotifyTitleGrabber
             InitializeComponent();
             DetectSpotify();
 
-            if (Properties.Settings.Default.ListPath.Length > 0)
-                tbxSongListFile.Text = Properties.Settings.Default.ListPath;
+            if (!File.Exists(Environment.CurrentDirectory + "\\config.cfg"))
+            {
+                var cfgx = new Configuration();
+
+                cfgx.ListPath = tbxSongListFile.Text;
+                cfgx.CurrentSongPath = tbxCurrentSongFile.Text;
+                cfgx.TitleFormat = tbxTitleFormat.Text;
+                cfgx.TitleFormatNoRemix = tbxTitleFormatNoRemix.Text;
+                cfgx.SaveToFile(Environment.CurrentDirectory + "\\config.cfg");
+            }       
+
+            var cfg = Configuration.FromFile(Environment.CurrentDirectory + "\\config.cfg");
+
+            if (cfg.ListPath.Length > 0)
+                tbxSongListFile.Text = cfg.ListPath;
             else tbxSongListFile.Text = Environment.CurrentDirectory + "\\SongList.txt";
-            if (Properties.Settings.Default.CurrentSongPath.Length > 0)
-                tbxCurrentSongFile.Text = Properties.Settings.Default.CurrentSongPath;
+            if (cfg.CurrentSongPath.Length > 0)
+                tbxCurrentSongFile.Text = cfg.CurrentSongPath;
             else tbxCurrentSongFile.Text = Environment.CurrentDirectory + "\\CurrentSong.txt";
 
-            if (Properties.Settings.Default.TitleFormat.Length > 0)
-                tbxTitleFormat.Text = Properties.Settings.Default.TitleFormat;
-            if (Properties.Settings.Default.TitleFormatNoRemix.Length > 0)
-                tbxTitleFormatNoRemix.Text = Properties.Settings.Default.TitleFormatNoRemix;
+            if (cfg.TitleFormat.Length > 0)
+                tbxTitleFormat.Text = cfg.TitleFormat;
+            if (cfg.TitleFormatNoRemix.Length > 0)
+                tbxTitleFormatNoRemix.Text = cfg.TitleFormatNoRemix;
 
             saveCurrentFileDialog.InitialDirectory = Environment.CurrentDirectory;
             saveListFileDialog.InitialDirectory = Environment.CurrentDirectory;
+        }
+
+        private void tmrNameRefresh_Tick(object sender, EventArgs e)
+        {
+            if (spotifyProcess != null)
+            {
+                spotifyProcess.Refresh();
+                if (spotifyProcess.HasExited)
+                    DetectSpotify();
+                else if (spotifyProcess.MainWindowTitle == "Spotify")
+                {
+                    titleName = "Currently no song playing!";
+                    if (titleName != titleNameOld)
+                        RefreshName(titleName);
+                }
+                else if (spotifyProcess.MainWindowTitle == "Drag")
+                    return;
+                else
+                {
+                    titleName = spotifyProcess.MainWindowTitle;
+                    if (titleName != titleNameOld)
+                        RefreshName(titleName);
+                }
+            }
+            else DetectSpotify();
         }
 
         public void DetectSpotify()
@@ -63,31 +102,6 @@ namespace SpotifyTitleGrabber
             File.AppendAllText(tbxSongListFile.Text, titleName + Environment.NewLine);
         }
 
-        private void tmrNameRefresh_Tick(object sender, EventArgs e)
-        {
-            if (spotifyProcess != null)
-            {
-                spotifyProcess.Refresh();
-                if (spotifyProcess.HasExited)
-                    DetectSpotify();
-                else if (spotifyProcess.MainWindowTitle == "Spotify")
-                {
-                    titleName = "Currently no song playing!";
-                    if (titleName != titleNameOld)
-                        RefreshName(titleName);
-                }
-                else if (spotifyProcess.MainWindowTitle == "Drag")
-                    return;
-                else
-                {
-                    titleName = spotifyProcess.MainWindowTitle;
-                    if (titleName != titleNameOld)
-                        RefreshName(titleName);
-                }
-            }
-            else DetectSpotify();
-        }
-
         private void btnCurrentSongFile_Click(object sender, EventArgs e)
         {
             DialogResult crntFile = saveCurrentFileDialog.ShowDialog();
@@ -96,7 +110,6 @@ namespace SpotifyTitleGrabber
                 if (saveCurrentFileDialog.FileName != tbxCurrentSongFile.Text & File.Exists(tbxCurrentSongFile.Text) & !File.Exists(saveCurrentFileDialog.FileName))
                     File.Move(tbxCurrentSongFile.Text, saveCurrentFileDialog.FileName);
                 tbxCurrentSongFile.Text = saveCurrentFileDialog.FileName;
-                Properties.Settings.Default.CurrentSongPath = saveCurrentFileDialog.FileName;
             }      
         }
 
@@ -108,7 +121,6 @@ namespace SpotifyTitleGrabber
                 if (saveListFileDialog.FileName != tbxSongListFile.Text & File.Exists(tbxSongListFile.Text) & !File.Exists(saveListFileDialog.FileName))
                     File.Move(tbxSongListFile.Text, saveListFileDialog.FileName);
                 tbxSongListFile.Text = saveListFileDialog.FileName;
-                Properties.Settings.Default.ListPath = saveListFileDialog.FileName;
             }
         }
 
@@ -126,9 +138,50 @@ namespace SpotifyTitleGrabber
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Properties.Settings.Default.TitleFormat = tbxTitleFormat.Text;
-            Properties.Settings.Default.TitleFormatNoRemix = tbxTitleFormatNoRemix.Text;
-            Properties.Settings.Default.Save();
+            var cfg = Configuration.FromFile(Environment.CurrentDirectory + "\\config.cfg");
+
+            cfg.ListPath = tbxSongListFile.Text;
+            cfg.CurrentSongPath = tbxCurrentSongFile.Text;
+            cfg.TitleFormat = tbxTitleFormat.Text;
+            cfg.TitleFormatNoRemix = tbxTitleFormatNoRemix.Text;
+
+            cfg.SaveToFile(Environment.CurrentDirectory + "\\config.cfg");
         }
+    }
+
+    [Serializable]
+    internal class Configuration
+    {
+        internal string CurrentSongPath { get; set; }
+        internal string ListPath { get; set; }
+        internal string TitleFormat { get; set; }
+        internal string TitleFormatNoRemix { get; set; }
+
+        internal bool IsValid => File.Exists(CurrentSongPath) && File.Exists(ListPath) && File.Exists(TitleFormat) && File.Exists(TitleFormatNoRemix);
+
+        #region Serialization Support
+
+        internal void SaveToFile(string filePath)
+        {
+            SaveToFile(new FileInfo(filePath));
+        }
+
+        internal void SaveToFile(FileInfo file)
+        {
+            using (FileStream fs = file.Open(FileMode.Create, FileAccess.Write))
+                new BinaryFormatter().Serialize(fs, this);
+        }
+
+        internal static Configuration FromFile(string filePath)
+        {
+            return FromFile(new FileInfo(filePath));
+        }
+
+        internal static Configuration FromFile(FileInfo file)
+        {
+            using (FileStream fs = file.Open(FileMode.Open, FileAccess.Read))
+                return (Configuration)new BinaryFormatter().Deserialize(fs);
+        }
+        #endregion
     }
 }
